ffc72c4f7092178dc7cc570fdab5be42ed0d01f1
V.levchenkov <V.levchenkovv@gmail.com>
2022-09-21 19:54:15 +0300

    правки под тест

--- src/test/java/ru/practicum/shareit/booking/controller/BookingControllerTest.java
+++ src/test/java/ru/practicum/shareit/booking/controller/BookingControllerTest.java
@@ -11,7 +11,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import ru.practicum.shareit.booking.bookingTestUtils.TestUtils;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingDtoSimple;
 import ru.practicum.shareit.booking.dto.BookingMapper;
